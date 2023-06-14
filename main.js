0b7a11e9f69256ec936a4623bfeaf0697c501090
QuirogaPau <paulaquiroga1207@gmail.com>
2023-06-13 23:41:05 -0300

    Fusion del menu en Desktop, manipulacion del DOM, creacion de archivo CSS

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuNav = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+menuNav.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+     desktopMenu.classList.toggle('inactive');
+     console.log('click')
+    
+}
